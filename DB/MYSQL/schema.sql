42f1e9e14644e4a803392bccf64da4943beeae4e
Dylan Ring <dylanRing.BS@gmail.com>
2020-12-05 12:57:48 -0700

    Changes to schema

--- DB/MYSQL/schema.sql
+++ DB/MYSQL/schema.sql
@@ -1,34 +1,34 @@
 
 -- start mysql and seed schema via the following
 -- sudo service mysql start
--- mysql -u root -p < schema.sql
+-- from root directory
+-- mysql -u root -p < ./DB/MYSQL/schema.sql
 
+DROP DATABASE sdc;
 CREATE DATABASE IF NOT EXISTS sdc;
 
 USE sdc;
 
-CREATE TABLE IF NOT EXISTS reservations (
-  resId int NOT NULL AUTO_INCREMENT,
-  listingKey int NOT NULL UNIQUE,
-  startDate DATE NOT NULL,
-  endDate DATE NOT NULL,
-  adults int,
-  children int,
-  infants int,
-  PRIMARY KEY (resId),
-  FOREIGN KEY (listingKey) REFERENCES listings(reservationKey)
-);
-
 CREATE TABLE IF NOT EXISTS listings (
-  listingId int NOT NULL AUTO_INCREMENT,
-  reservationKey int NOT NULL UNIQUE,
+  listingId int NOT NULL UNIQUE  AUTO_INCREMENT,
+  -- reservationKey int NOT NULL UNIQUE, get via join querry ??
   occupency int NOT NULL,
   feeNightly int,
   feeService int,
   feeCleaning int,
   rating float,
   numRatings int,
-  PRIMARY KEY (aptId)
+  PRIMARY KEY (listingId)
 );
 
-
+CREATE TABLE IF NOT EXISTS reservations (
+  resId int NOT NULL AUTO_INCREMENT,
+  listingKey int NOT NULL,
+  startDate DATE NOT NULL,
+  endDate DATE NOT NULL,
+  adults int,
+  children int,
+  infants int,
+  PRIMARY KEY (resId),
+  FOREIGN KEY (listingKey) REFERENCES listings(listingId)
+);
