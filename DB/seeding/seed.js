eb428bc96a8258e613fd4834706c0cfb0e8b2a92
Dylan Ring <dylanRing.BS@gmail.com>
2020-12-07 13:49:40 -0700

    CSV seed and mySQl seed working independently to 10M primary records

--- DB/seeding/seed.js
+++ DB/seeding/seed.js
@@ -1,13 +1,27 @@
-// runs all seeding files to create a csv file and and populate all databases
+// // runs all seeding files to create a csv file and and populate all databases
 
 
-const csvSeed = require('./seedCSV.js');
-const mySQlSeed = require('./populateMySQL.js');
-// cassandra
+// const csvSeed = require('./seedCSV.js');
+// const mySQlSeed = require('./populateMySQL.js');
+// // cassandra
 
-  // seed csv files function call
-  let main = async () => {
-    await csvSeed();
-    await mySqlSeed();
-  }
-  main();
+//   // seed csv files function call
+// // how to use async await without returning i.e. no const x = await function();
+
+//   let main = async () => {
+//     const temp1 = await csvSeed();
+//     console.log('\n------------\n');
+//     const temp2 = await mySqlSeed();
+//   };
+
+// // let main = () => {
+// //   csvSeed(() => {
+// //     console.log('\n------------\n');
+// //     mySQlSeed(() => {
+// //       console.log('Seed Complete!');
+// //     });
+// //   });
+// // };
+
+
+//   main();
