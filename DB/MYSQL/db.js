d3581c2e7f157d018cb56864c736a71f93813465
Dylan Ring <dylanRing.BS@gmail.com>
2020-12-07 17:56:05 -0700

    Post Request written but untested need to fix auto increment

--- DB/MYSQL/db.js
+++ DB/MYSQL/db.js
@@ -59,6 +59,40 @@ const getAllResForListing = (listingKey, callback) => {
 
 // posts
 
+// Primary Ids should be set by db !!!
+const newRecord = (table, data, callback) => {
+  let query;
+  let values;
+  if (table = 'listing') {
+    query = `INSERT INTO ${table} (listingId, occupancy, feeeNightly, feeService, feeCleaning, rating, numRatings) VAlUES (?, ?, ?, ?, ?, ?, ?)`;
+    values = [data.listingId, data.occupancy, data.feeeNightly, data.feeService, data.feeCleaning, data.rating, data.numRatings];
+  }  else if (table = 'reservation') {
+    query = `INSERT INTO ${type} (resId, listingKey, startDate, endDate, adults, children, infants) Values (?, ?, ?, ?, ?, ?, ?)`;
+    values = [data.resId, data.listingKey, data.startDate, data.endDate, data.adults, data.children, data.infants];
+  } else {
+    console.error('Error type in newRecord');
+    callback(404, null);
+  }
+  connection.query(query, values, (err, res) => {
+    if (err) {
+      console.error('Error in newRecord');
+      callback(err, res);
+    }
+    callback(null, res);
+  });
+};
+
+const newListing = (data, callback) => {
+  let query = `INSERT INTO reservations (listingId, occupancy, feeeNightly, feeService, feeCleaning, rating, numRatings) VAlUES (?, ?, ?, ?, ?, ?, ?)`;
+  let values = [data.listingId, data.occupancy, data.feeeNightly, data.feeService, data.feeCleaning, data.rating, data.numRatings];
+  connection.query(query, values, (err, res) => {
+    if (err) {
+      console.error('Error in newListing');
+      callback(err, res);
+    }
+    callback(null, res);
+  });
+};
 
 // update
 
