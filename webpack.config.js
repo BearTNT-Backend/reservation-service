3580f7ab704f9b2af4b55bedc0ccf4329f8c2960
Dylan Ring <dylanRing.BS@gmail.com>
2020-12-18 17:14:31 -0700

    Some optimization of webpack bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
-
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env':{'NODE_ENV': JSON.stringify('production'),}
+    }),
+  ],
   entry: SRC_DIR +'/index.jsx',
   output: {
     filename: 'bundle.js',
